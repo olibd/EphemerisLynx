c88df9222c9ef51f69b204851a2e10b734a1a91b
Olivier Brochu Dufour <olibd@outlook.com>
2017-06-05 18:06:47 -0400

    #116 — Simple, basic and generic identity map to be used by the mappers.

---
+++ Lynx.Mobile/Lynx.Core/Mappers/IdentityMap.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Lynx.Core.Mappers
+{
+    public class IdentityMap<K, V>
+    {
+        private Dictionary<K, V> _idMap;
+
+        public IdentityMap()
+        {
+        }
+
+        /// <summary>
+        /// Find the value for specified key.
+        /// </summary>
+        /// <returns>The found value. default(V) if not found.</returns>
+        /// <param name="key">Key.</param>
+        public V Find(K key)
+        {
+            if (_idMap.ContainsKey(key))
+                return _idMap[key];
+            else
+                return default(V);
+        }
+
+        /// <summary>
+        /// Add the specified Value to the specified Key.
+        /// </summary>
+        /// <returns>void</returns>
+        /// <param name="key">Key.</param>
+        /// <param name="val">Value.</param>
+        public void Add(K key, V val)
+        {
+            _idMap.Add(key, val);
+        }
+    }
+}
