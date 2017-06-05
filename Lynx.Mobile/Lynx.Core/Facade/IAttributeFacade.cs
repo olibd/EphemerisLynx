c864d690392f078247222127c1fe4e736e6fe445
jmontineri <jmontineri@gmail.com>
2017-06-05 18:24:09 -0400

    Made required facade interface changes + stripped features down to US requirements

---
+++ Lynx.Mobile/Lynx.Core/Facade/IAttributeFacade.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections.Generic;
+
+namespace Lynx.Core.Facade
+{
+    interface IAttributeFacade
+    {
+        /// <summary>
+        /// Creates a new attribute
+        /// </summary>
+        /// <param name="model"></param>
+        void Deploy(AttributeModel model);
+
+        /// <summary>
+        /// Returns a new AttributeModel using the data contained in the contract at the address specified.
+        /// </summary>
+        /// <param name="address">The Attribute contract address</param>
+        /// <returns>The Attribute at this address</returns>
+        AttributeModel GetAttribute(string address);
+
+        /// <summary>
+        /// Gets the list of certificates in the attribute
+        /// </summary>
+        /// <returns>Certificates</returns>
+        List<CertificateModel> GetCertificates(AttributeModel attribute);
+    }
+
+
+}
---
+++ Lynx.Mobile/Lynx.Core/Facade/ICertificateFacade.cs
@@ -0,0 +1,15 @@
+﻿namespace Lynx.Core.Facade
+{
+
+    interface ICertificateFacade
+    {
+        /// <summary>
+        /// Returns a new CertificateModel using the data contained in the contract at the address specified.
+        /// </summary>
+        /// <param name="address">The certificate contract address</param>
+        /// <returns>The certificate at this address</returns>
+        CertificateModel GetCertificate(string address);
+    }
+
+
+}
