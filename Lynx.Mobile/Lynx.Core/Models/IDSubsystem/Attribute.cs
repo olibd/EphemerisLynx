e66c09c6da8d1072946c9b209664d42fdb9fe43b
Olivier Brochu Dufour <olibd@outlook.com>
2017-06-05 15:01:27 -0400

    #105 #106 #107 #108 — Minimal implementation of the aforementioned task to satisfy the current user story

---
+++ Lynx.Mobile/Lynx.Core/Models/IDSubsystem/Attribute.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Lynx.Core.Models.IDSubsystem
+{
+    public class Attribute<T> : ExternalElement<T>
+    {
+        private Dictionary<string, Certificate<T>> _certificates;
+
+        public Attribute()
+        {
+        }
+
+        public void AddCertificate(Certificate<T> attr)
+        {
+            _certificates.Add(attr.Hash, attr);
+        }
+
+        public Certificate<T> GetCertificate(string hash)
+        {
+            return _certificates[hash];
+        }
+    }
+}
---
+++ Lynx.Mobile/Lynx.Core/Models/IDSubsystem/ExternalElement.cs
@@ -0,0 +1,32 @@
+﻿using System;
+namespace Lynx.Core.Models.IDSubsystem
+{
+    public class ExternalElement<T> : SmartContract
+    {
+        public string Location { get; set; }
+        public string Hash { get; set; }
+        public T Content { get; set; }
+
+        public ExternalElement()
+        {
+        }
+
+        public ExternalElement(String location)
+        {
+            Location = location;
+        }
+
+        public ExternalElement(string location, string hash)
+        {
+            Location = location;
+            Hash = hash;
+        }
+
+        public ExternalElement(string location, string hash, T content)
+        {
+            Location = location;
+            Hash = hash;
+            Content = content;
+        }
+    }
+}
