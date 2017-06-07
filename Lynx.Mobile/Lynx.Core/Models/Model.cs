6e7ea28ec7d8e4eb9cf694e7ca0b5fa23e1975c8
Olivier Brochu Dufour <olibd@outlook.com>
2017-06-07 12:23:32 -0400

    Common model class to Standardize model classes (helpful for DB operations)

---
+++ Lynx.Mobile/Lynx.Core/Models/Model.cs
@@ -0,0 +1,12 @@
+﻿using System;
+namespace Lynx.Core.Models
+{
+    public abstract class Model
+    {
+        /// <summary>
+        /// Gets or sets the MUID (Model Unique ID).
+        /// </summary>
+        /// <value>The MUID.</value>
+        private int MUID { get; set; }
+    }
+}
