b224f677d4918415a54227ff15b7a8a34c01fe26
Olivier Brochu Dufour <olibd@outlook.com>
2017-07-03 13:28:00 -0400

    Added ignore attribute to non SQlite.Net serializable property

--- Lynx.Mobile/Lynx.Core/Models/IDSubsystem/Certificate.cs
+++ Lynx.Mobile/Lynx.Core/Models/IDSubsystem/Certificate.cs
@@ -1,10 +1,12 @@
 ﻿using System;
+using SQLite;
 
 namespace Lynx.Core.Models.IDSubsystem
 {
     public class Certificate : ExternalElement
     {
         public bool Revoked { get; set; }
+        [Ignore]
         public Attribute OwningAttribute { get; set; }
     }
 }
