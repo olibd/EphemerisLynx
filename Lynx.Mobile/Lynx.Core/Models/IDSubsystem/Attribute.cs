9d4b6798c0610ccf3ca046eeda67d801b7dc2930
Olivier Brochu Dufour <olibd@outlook.com>
2017-07-05 13:01:24 -0400

    Added a description property to the attribute object that can be used to define what does the attribute does ex: firstname, last name, etc…

--- Lynx.Mobile/Lynx.Core/Models/IDSubsystem/Attribute.cs
+++ Lynx.Mobile/Lynx.Core/Models/IDSubsystem/Attribute.cs
@@ -6,6 +6,8 @@ namespace Lynx.Core.Models.IDSubsystem
 {
     public class Attribute : ExternalElement
     {
+        public String Description { get; set; }
+
         [Ignore]
         public Dictionary<string, Certificate> Certificates { get; set; }
 
