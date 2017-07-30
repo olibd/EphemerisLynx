a3b39f47a4e144b4435ad1d922cf4c79764afbd3
Jonathan Montineri <jmontineri@gmail.com>
2017-07-30 13:01:52 -0400

    Added unit test to project

---
+++ Lynx.Mobile/CoreUnitTests.PCL/SessionTest.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using Lynx.Core.Services.Interfaces;
+using Lynx.Core.Services;
+using NUnit.Framework;
+using NUnit.Framework.Internal;
+using Java.Lang;
+
+namespace CoreUnitTests
+{
+    [TestFixture]
+    public class SessionTest 
+    {
+        private ISession _session1;
+        private ISession _session2;
+
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void TestSendAndReceiveMessage()
+        {
+            ManualResetEvent resetEvent = new ManualResetEvent(false);
+            string messageReceived = "";
+            string message = "This is a test message";
+
+            _session1 = new Session(delegate{});
+            _session2 = new Session(
+                delegate(object sender, string eventArgs)
+                {
+                    resetEvent.Set();
+                    messageReceived = eventArgs;
+                }
+            );
+;
+            string sessionKey = _session1.Open();
+            _session2.Open(sessionKey);
+
+            Thread.Sleep(2000);
+
+            _session1.Send(message);
+            resetEvent.WaitOne(5000);
+
+            Assert.AreEqual(message, messageReceived);
+        }
+
+    }
+}
