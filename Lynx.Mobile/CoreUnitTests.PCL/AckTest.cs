1cfa122d9f4ad8aafa6316f3e913ee61db105928
Olivier Brochu Dufour <olibd@outlook.com>
2017-08-01 17:08:09 -0400

    Added the unit test for the Task191.

---
+++ Lynx.Mobile/CoreUnitTests.PCL/AckTest.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using Lynx.Core.Models.IDSubsystem;
+using Lynx.Core.Services;
+using Newtonsoft.Json;
+using NUnit.Framework;
+
+namespace CoreUnitTests.PCL
+{
+    [TestFixture()]
+    public class AckTest : HandshakeTokenTest
+    {
+        [SetUp]
+        public override void Setup()
+        {
+            base.Setup();
+            IContent name = new StringContent("Olivier");
+            _payload.Add("name", JsonConvert.SerializeObject(name));
+            _token = new Ack()
+            {
+                Encrypted = Boolean.Parse(_header["encrypted"]),
+                PublicKey = _header["pubkey"],
+                Name = name,
+                Id = _id
+            };
+        }
+    }
+}
