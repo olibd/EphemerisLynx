a6535d1e02dd1684d87619edb480fdf0820fc60a
Lina Nouh <lina.nouh@gmail.com>
2017-08-05 00:17:22 -0400

    Added elliptic curve encryption and decryption + test

--- Lynx.Mobile/Lynx.Core/Services/SECP256K1CryptoService.cs
+++ Lynx.Mobile/Lynx.Core/Services/SECP256K1CryptoService.cs
@@ -8,39 +8,124 @@ using Org.BouncyCastle.Crypto.Agreement.Kdf;
 using Org.BouncyCastle.Crypto.Parameters;
 using Org.BouncyCastle.Math;
 using Org.BouncyCastle.Security;
+using Org.BouncyCastle.Asn1.Sec;
+using Org.BouncyCastle.Math.EC;
+using System.Linq;
 
 namespace Lynx.Core.Services
 {
-    public class SECP256K1CryptoService : ISECP256K1CryptoService
-    {
-        private string _curveName = "secp256k1";
-        private X9ECParameters _ecP;
-        private ECDomainParameters _ecSpec;
-        private ISigner _signer = SignerUtilities.GetSigner("SHA-256withECDSA");
+	public class SECP256K1CryptoService : ISECP256K1CryptoService
+	{
+		private string _curveName = "secp256k1";
+		private X9ECParameters _ecP;
+		private ECDomainParameters _ecSpec;
+		private ISigner _signer = SignerUtilities.GetSigner("SHA-256withECDSA");
 
-        public SECP256K1CryptoService()
+		public SECP256K1CryptoService()
+		{
+			_ecP = SecNamedCurves.GetByName(_curveName);
+			_ecSpec = new ECDomainParameters(_ecP.Curve, _ecP.G, _ecP.N, _ecP.H, _ecP.GetSeed());
+		}
+
+		public byte[] GetDataSignature(byte[] data, byte[] privateKey)
+		{
+			//Adapted From: https://stackoverflow.com/questions/17439732/recreating-keys-ecpublickeyparameters-in-c-sharp-with-bouncycastle
+			BigInteger biPrivateKey = new BigInteger(privateKey);
+			ECPrivateKeyParameters keyParameters = new ECPrivateKeyParameters(biPrivateKey, _ecSpec);
+			_signer.Init(true, keyParameters);
+			_signer.BlockUpdate(data, 0, data.Length);
+			return _signer.GenerateSignature();
+		}
+
+		public bool VerifySignedData(byte[] data, byte[] signature, byte[] pubkey)
+		{
+			ECPublicKeyParameters ecpubKey = (ECPublicKeyParameters)PublicKeyFactory.CreateKey(pubkey);
+			_signer.Init(false, ecpubKey);
+			_signer.BlockUpdate(data, 0, data.Length);
+			return _signer.VerifySignature(signature);
+		}
+
+		public ECPublicKeyParameters GeneratePublicKey(byte[] publickey)
         {
-            _ecP = NistNamedCurves.GetByName(_curveName);
-            _ecSpec = new ECDomainParameters(_ecP.Curve, _ecP.G, _ecP.N, _ecP.H, _ecP.GetSeed());
-        }
+			FpCurve c = (FpCurve)_ecP.Curve;
+			ECPoint point = _ecSpec.Curve.DecodePoint(publickey);
+			ECPublicKeyParameters publicKey = new ECPublicKeyParameters("ECDH", point, _ecSpec);
+			return publicKey;
+		}
 
-        public byte[] GetDataSignature(byte[] data, byte[] privateKey)
-        {
-            //Adapted From: https://stackoverflow.com/questions/17439732/recreating-keys-ecpublickeyparameters-in-c-sharp-with-bouncycastle
-            BigInteger biPrivateKey = new BigInteger(privateKey);
-            ECPrivateKeyParameters keyParameters = new ECPrivateKeyParameters(biPrivateKey, _ecSpec);
-            _signer.Init(true, keyParameters);
-            _signer.BlockUpdate(data, 0, data.Length);
-            return _signer.GenerateSignature();
-        }
+		public ECPrivateKeyParameters GeneratePrivateKey(BigInteger q)
+		{
+			ECPrivateKeyParameters privateKey = new ECPrivateKeyParameters("ECDH", q, _ecSpec);
+			return privateKey;
+		}
 
-        public bool VerifySignedData(byte[] data, byte[] signature, byte[] pubkey)
-        {
-            ECPublicKeyParameters ecpubKey = (ECPublicKeyParameters)PublicKeyFactory.CreateKey(pubkey);
-            _signer.Init(false, ecpubKey);
-            _signer.BlockUpdate(data, 0, data.Length);
-            return _signer.VerifySignature(signature);
-        }
+		public byte[] GetSharedSecretValue(ECPrivateKeyParameters privatekey, ECPublicKeyParameters publickey)
+		{
+			ECDHCBasicAgreement eLacAgreement = new ECDHCBasicAgreement();
+			eLacAgreement.Init(privatekey);
+			BigInteger eLA = eLacAgreement.CalculateAgreement(publickey);
+			return eLA.ToByteArray();
+		}
 
-    }
+		//From that shared secret key, generate an encryption key
+		public byte[] DeriveSymmetricKeyFromSharedSecret(byte[] sharedSecret)
+		{
+			Org.BouncyCastle.Crypto.Agreement.Kdf.ECDHKekGenerator egH =
+				   new ECDHKekGenerator(DigestUtilities.GetDigest("SHA256"));
+			egH.Init(new DHKdfParameters(NistObjectIdentifiers.Aes, sharedSecret.Length, sharedSecret));
+			byte[] symmetricKey = new byte[DigestUtilities.GetDigest("SHA256").GetDigestSize()];
+			egH.GenerateBytes(symmetricKey, 0, symmetricKey.Length);
+			return symmetricKey;
+		}
+
+		public byte[] Encrypt(byte[] data, byte[] derivedKey)
+		{
+			byte[] output = null;
+			try
+			{
+				KeyParameter keyparam = ParameterUtilities.CreateKeyParameter("DES", derivedKey);
+				IBufferedCipher cipher = CipherUtilities.GetCipher("DES/ECB/ISO7816_4PADDING");
+				cipher.Init(true, keyparam);
+				try
+				{
+					output = cipher.DoFinal(data);
+					return output;
+				}
+				catch (System.Exception ex)
+				{
+					throw new CryptoException("Invalid Data");
+				}
+			}
+			catch (Exception ex)
+			{
+
+			}
+			return output;
+		}
+
+		public byte[] Decrypt(byte[] cipherData, byte[] derivedKey)
+		{
+			byte[] output = null;
+			try
+			{
+				KeyParameter keyparam = ParameterUtilities.CreateKeyParameter("DES", derivedKey);
+				IBufferedCipher cipher = CipherUtilities.GetCipher("DES/ECB/ISO7816_4PADDING");
+				cipher.Init(false, keyparam);
+				try
+				{
+					output = cipher.DoFinal(cipherData);
+
+				}
+				catch (System.Exception ex)
+				{
+					throw new CryptoException("Invalid Data");
+				}
+			}
+			catch (Exception ex)
+			{
+			}
+			return output;
+		}
+	}
 }
+
