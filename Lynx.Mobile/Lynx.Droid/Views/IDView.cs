6c582dc8d7ee3c76c685a2e0dc6e9154185d895a
Jonathan Montineri <jmontineri@gmail.com>
2017-08-11 19:38:36 -0400

    Added ZXing scanner (first iteration, buggy, no callbacks)

--- Lynx.Mobile/Lynx.Droid/Views/IDView.cs
+++ Lynx.Mobile/Lynx.Droid/Views/IDView.cs
@@ -1,6 +1,8 @@
-﻿using Android.App;
+﻿using System;
+using Android.App;
 using Android.OS;
 using Android.Support.Design.Widget;
+using Android.Support.V7.Widget;
 using Android.Widget;
 using Lynx.Core.Models.Interactions;
 using Lynx.Core.ViewModels;
@@ -8,17 +10,24 @@ using Lynx.Droid.Views.Callbacks;
 using MvvmCross.Binding.BindingContext;
 using MvvmCross.Core.ViewModels;
 using MvvmCross.Droid.Views;
+using MvvmCross.Droid.Support.V4;
 using MvvmCross.Platform.Core;
+using ZXing;
+using ZXing.Mobile;
+using ZXing.Net.Mobile.Android;
 using static Android.Support.Design.Widget.BottomSheetBehavior;
 
 namespace Lynx.Droid.Views
 {
     [Activity(Label = "View for IDViewModel")]
-    public class IDView : MvxActivity
+    public class IDView : MvxFragmentActivity
     {
         private BottomSheetBehavior _bottomSheetBehavior;
         private LinearLayout _bottomSheet;
         private CoordinatorLayout _IDViewLayout;
+
+        private ZXingScannerFragment _scanner;
+
         protected override void OnCreate(Bundle bundle)
         {
             base.OnCreate(bundle);
@@ -27,6 +36,35 @@ namespace Lynx.Droid.Views
             _bottomSheetBehavior = BottomSheetBehavior.From(_bottomSheet);
             _IDViewLayout = FindViewById<CoordinatorLayout>(Resource.Id.IDViewLayout);
             _bottomSheetBehavior.SetBottomSheetCallback(new BottomSheetInvalidateParentCallback());
+
+            _scanner = new ZXingScannerFragment();
+            _scanner.ScanningOptions = MobileBarcodeScanningOptions.Default;
+            SupportFragmentManager.BeginTransaction()
+                .Add(Resource.Id.IDViewLayout, _scanner, "ZXINGSCANNER")
+                .Commit();
+            
+        }
+
+        protected override void OnResume()
+        {
+            base.OnResume();
+            if (PermissionsHandler.NeedsPermissionRequest(this))
+            {
+                PermissionsHandler.RequestPermissionsAsync(this);
+            }
+            Handler handler = new Handler(Looper.MainLooper);
+
+            _scanner.StartScanning(result =>
+            {
+                handler.Post(() =>
+                {
+                    Toast toast = new Toast(this);
+                    toast.Duration = ToastLength.Short;
+                    toast.SetText(result.Text);
+                    toast.Show();
+                });
+            }, MobileBarcodeScanningOptions.Default);
         }
     }
 }
+
