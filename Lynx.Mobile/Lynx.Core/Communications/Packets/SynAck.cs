a2aca99ecbbc1af702f5806af4a21e956d2d515e
Olivier Brochu Dufour <olibd@outlook.com>
2017-08-16 00:00:32 -0400

    Created the packet for SynAck and ISynAck. Also moved the token decryption methods of the Syn and Ack class into a parent Peer class and renamed it ProcessEncryptHandshakeToken.

---
+++ Lynx.Mobile/Lynx.Core/Communications/Packets/SynAck.cs
@@ -0,0 +1,9 @@
+﻿using System;
+using Lynx.Core.Communications.Packets.Interfaces;
+
+namespace Lynx.Core.Communications.Packets
+{
+    public class SynAck : HandshakeToken, ISynAck
+    {
+    }
+}
---
+++ Lynx.Mobile/Lynx.Core/PeerVerification/Peer.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Threading.Tasks;
+using Lynx.Core.Communications.Packets;
+using Lynx.Core.Communications.Packets.Interfaces;
+using Lynx.Core.Crypto.Interfaces;
+using Lynx.Core.Facade.Interfaces;
+using Lynx.Core.Interfaces;
+
+namespace Lynx.Core.PeerVerification
+{
+    public abstract class Peer
+    {
+        private ITokenCryptoService<IHandshakeToken> _tokenCryptoService;
+        private IAccountService _accountService;
+        private IIDFacade _idFacade;
+
+        public Peer(ITokenCryptoService<IHandshakeToken> tokenCryptoService, IAccountService accountService, IIDFacade idFacade)
+        {
+            _accountService = accountService;
+            _tokenCryptoService = tokenCryptoService;
+            _idFacade = idFacade;
+        }
+
+        /// <summary>
+        /// Decrypt and parses a JSON-encoded HandshakeToken
+        /// </summary>
+        /// <param name="encryptedHandshakeToken">The JSON-encoded SYNACK</param>
+        /// <returns>The HandshakeToken object</returns>
+        protected virtual async Task<T> ProcessEncryptedHandshakeToken<T>(string encryptedHandshakeToken) where T : HandshakeToken, new()
+        {
+            string decryptedToken = _tokenCryptoService.Decrypt(encryptedHandshakeToken, _accountService.GetPrivateKeyAsByteArray());
+            HandshakeTokenFactory<T> ackTokenFactory = new HandshakeTokenFactory<T>(_idFacade);
+            T handshakeToken = await ackTokenFactory.CreateHandshakeTokenAsync(decryptedToken);
+
+            //Compare public address derived from the public key used to encrypt 
+            //the token with the public address used to control the ID specify
+            //in the token header. If they match then the person sending/encrypting
+            //the token is the same as the one controlling the ID specified in
+            //the header.
+            string tokenPublicAddress = AccountService.GeneratePublicAddressFromPublicKey(handshakeToken.PublicKey);
+
+            if (tokenPublicAddress != handshakeToken.Id.Owner)
+                throw new TokenSenderIsNotIDOwnerException();
+
+            return handshakeToken;
+        }
+    }
+}
