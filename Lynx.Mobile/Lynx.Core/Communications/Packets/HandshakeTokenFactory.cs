70459ed348aee5b4b29d522ecb2810e3a7ebfb88
Olivier Brochu Dufour <olibd@outlook.com>
2017-08-25 10:44:23 -0400

    Modified the Handshake token factory so that it passes an unlocked token to the base. This allows it the then modify the returned token and then lock it

--- Lynx.Mobile/Lynx.Core/Communications/Packets/HandshakeTokenFactory.cs
+++ Lynx.Mobile/Lynx.Core/Communications/Packets/HandshakeTokenFactory.cs
@@ -18,24 +18,51 @@ namespace Lynx.Core.Communications.Packets
 
         public async Task<T> CreateHandshakeTokenAsync(string encodedToken)
         {
-            T t = base.CreateToken(encodedToken);
-
             //decode header to get the ID address and the accessible attribtue
             string[] splittedEncodedToken = encodedToken.Split('.');
+
+            ///////////////////////////////////////////////////////////////////
+            //If the token has a signature, then the base will return a locked
+            //token, we need to extract the signature from the encoded token
+            //supplied to the base. And we'll add the signature in this method
+            //instead
+            ///////////////////////////////////////////////////////////////////
+            string signature = null;
+            if (splittedEncodedToken.Length == 3)
+            {
+                //save a copy of the signature
+                signature = splittedEncodedToken[2];
+                //remove the signature from the encodedToken
+                encodedToken = splittedEncodedToken[0] + "." + splittedEncodedToken[1];
+            }
+
+            T t = base.CreateToken(encodedToken);
+
+            ///////////////////////////////////////////////////////////////////
+            //Instantiate the ID object from the address found in the payload//
+            ///////////////////////////////////////////////////////////////////
             string jsonDecodedPayload = Base64Decode(splittedEncodedToken[1]);
             Dictionary<string, string> payload = JsonConvert.DeserializeObject<Dictionary<string, string>>(jsonDecodedPayload);
 
             ID id;
+
             //get the accessible attributes
             if (payload.ContainsKey("accAttr"))
             {
+                //Attempt to load the ID with only the accessible attributes
                 string[] accessibleAttributes = JsonConvert.DeserializeObject<string[]>(payload["accAttr"]);
                 id = await _idFacade.GetIDAsync(payload["idAddr"], accessibleAttributes);
             }
             else
+                //Attempt to load the ID without all attributes
                 id = await _idFacade.GetIDAsync(payload["idAddr"]);
 
             t.Id = id;
+
+            //If there was a signature in the encoded token, we sign it
+            if (signature != null)
+                t.SignAndLock(signature);
+
             return t;
         }
     }
