4fb76b524e340fa95c98039658a080144f879122
Olivier Brochu Dufour <olibd@outlook.com>
2017-09-01 17:18:38 -0400

    Wired the verifier to the CertifyVieModel

--- Lynx.Mobile/Lynx.Core/ViewModels/CertifyViewModel.cs
+++ Lynx.Mobile/Lynx.Core/ViewModels/CertifyViewModel.cs
@@ -7,58 +7,61 @@ using Attribute = Lynx.Core.Models.IDSubsystem.Attribute;
 using MvvmCross.Core.Navigation;
 using System.Threading.Tasks;
 using Lynx.Core.Communications.Packets;
+using Lynx.Core.PeerVerification.Interfaces;
 
 namespace Lynx.Core.ViewModels
 {
-    public class CertifyViewModel : MvxViewModel<ID>
+    public class CertifyViewModel : MvxViewModel<IVerifier>
     {
-		public ID ID { get; set; }
-		public List<Attribute> Attributes { get; set; }
+        public ID ID { get; set; }
+        public List<Attribute> Attributes { get; set; }
         public SynAck _synAck;
-        public List<string> certifiedAttributes;
-		private IMvxNavigationService _navigationService;
+        public List<string> attributesToCertify;
+        private IMvxNavigationService _navigationService;
+        private IVerifier _verifier;
 
-		public CertifyViewModel(IMvxNavigationService navigationService)
+        public CertifyViewModel(IMvxNavigationService navigationService)
         {
             _navigationService = navigationService;
         }
 
-		public override Task Initialize(ID Id)
-		{
-            ID = Id;
+        public override Task Initialize(IVerifier verifier)
+        {
+            _verifier = verifier;
+            ID = _verifier.SynAck.Id;
             Attributes = ID.Attributes.Values.ToList();
             return base.Initialize();
-		}
+        }
 
-		public override void Start()
-		{
-			//TODO: Add starting logic here
-		}
+        public override void Start()
+        {
+            //TODO: Add starting logic here
+        }
 
         public void UpdateCertifiedAttributes()
         {
-            
+
         }
 
-		public IMvxCommand UpdateCertifiedAttributesCommand => new MvxCommand<string>(UpdateCertifiedAttributes);
+        public IMvxCommand UpdateCertifiedAttributesCommand => new MvxCommand<string>(UpdateCertifiedAttributes);
 
         public void UpdateCertifiedAttributes(string attributeDescription)
         {
-			if (certifiedAttributes.Contains(attributeDescription))
-			{
-				certifiedAttributes.Remove(attributeDescription);
-			}
-			else
-			{
-				certifiedAttributes.Add(attributeDescription);
-			}
+            if (attributesToCertify.Contains(attributeDescription))
+            {
+                attributesToCertify.Remove(attributeDescription);
+            }
+            else
+            {
+                attributesToCertify.Add(attributeDescription);
+            }
         }
 
         public IMvxCommand CertifyIDCommand => new MvxCommand(CertifyID);
 
-		private void CertifyID()
-		{
-			throw new NotImplementedException();
-		}
+        private void CertifyID()
+        {
+            _verifier.Certify(attributesToCertify.ToArray());
+        }
     }
 }
