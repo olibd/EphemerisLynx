9a01d6ab8ba5adf3055ea118a126cfe1c94a418b
Jonathan Montineri <jmontineri@gmail.com>
2017-09-03 14:25:56 -0400

    Submodule changes, unit test fixes

--- Lynx.Mobile/CoreUnitTests.PCL/RequesterTest.cs
+++ Lynx.Mobile/CoreUnitTests.PCL/RequesterTest.cs
@@ -8,6 +8,7 @@ using Lynx.Core.Crypto;
 using Lynx.Core.Crypto.Interfaces;
 using Lynx.Core.Facade;
 using Lynx.Core.Facade.Interfaces;
+using Lynx.Core.Interfaces;
 using Lynx.Core.Models.IDSubsystem;
 using Lynx.Core.PeerVerification;
 using Nethereum.Web3;
@@ -66,14 +67,6 @@ namespace CoreUnitTests.PCL
             _id.AddAttribute("Age", age);
             _id.Address = "0x1234567";
 
-            ////////////////////////
-            //Create the ID Facade//
-            ////////////////////////
-            initWeb3().Wait();
-            _certFacade = new CertificateFacade(_addressFrom, "", _web3, new DummyContentService());
-            _attributeFacade = new AttributeFacade(_addressFrom, "", _web3, _certFacade, new DummyContentService());
-            _idFacade = new IDFacade(_addressFrom, "", "0x0", _web3, _attributeFacade);
-
             /////////////////////////
             //Create an eth account//
             /////////////////////////
@@ -81,6 +74,15 @@ namespace CoreUnitTests.PCL
             _accountService = new AccountService(_privateKey);
             _tokenCryptoService = new TokenCryptoService<IHandshakeToken>(new SECP256K1CryptoService());
             _requester = new Requester(_tokenCryptoService, _accountService, _id, _idFacade);
+
+            ////////////////////////
+            //Create the ID Facade//
+            ////////////////////////
+            initWeb3().Wait();
+            _certFacade = new CertificateFacade(_web3, new DummyContentService(), _accountService);
+            _attributeFacade = new AttributeFacade(_web3, _certFacade, new DummyContentService(), _accountService);
+            _idFacade = new IDFacade("0x0", _web3, _attributeFacade, _accountService);
+
         }
 
         [Test]
@@ -130,8 +132,8 @@ namespace CoreUnitTests.PCL
 
         private async Task initWeb3()
         {
-            _web3 = new Web3("http://10.0.1.11:8082");
-            _addressFrom = (await _web3.Eth.Accounts.SendRequestAsync())[0];
+            _web3 = new Web3("http://jmon.tech:8545");
+            _addressFrom = _accountService.GetAccountAddress();
         }
     }
 }
