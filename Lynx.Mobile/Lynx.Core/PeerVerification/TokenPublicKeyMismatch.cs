c7393d09a43f0bae7eeac4c700bcfbcb741e2f45
Olivier Brochu Dufour <olibd@outlook.com>
2017-09-08 14:48:19 -0400

    Added exception that should be triggered if in one handshake you receive token from 2 different public keys. Also added a class for the certificate events

---
+++ Lynx.Mobile/Lynx.Core/PeerVerification/TokenPublicKeyMismatch.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Lynx.Core.PeerVerification
+{
+    internal class TokenPublicKeyMismatch : Exception
+    {
+        public TokenPublicKeyMismatch()
+        {
+        }
+
+        public TokenPublicKeyMismatch(string message) : base(message)
+        {
+        }
+
+        public TokenPublicKeyMismatch(string message, Exception innerException) : base(message, innerException)
+        {
+        }
+    }
+}
