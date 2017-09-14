8374fd22816bba62434f78a817a72713f40c1c3e
Lina Nouh <lina.nouh@gmail.com>
2017-09-14 18:09:01 -0400

    Created CheckedAttribute class in CertifyViewModel to bind check boxes in CertifyAttributeView to attributes

--- Lynx.Mobile/Lynx.Core/ViewModels/CertifyViewModel.cs
+++ Lynx.Mobile/Lynx.Core/ViewModels/CertifyViewModel.cs
@@ -1,4 +1,4 @@
-using System;
+﻿using System;
 using System.Linq;
 using System.Collections.Generic;
 using Lynx.Core.Models.IDSubsystem;
@@ -11,47 +11,78 @@ using Lynx.Core.PeerVerification.Interfaces;
 
 namespace Lynx.Core.ViewModels
 {
-    public class CertifyViewModel : MvxViewModel<IVerifier>
-    {
-        public ID ID { get; set; }
-        public List<Attribute> Attributes { get; set; }
-        private List<string> _attributesToCertify;
-        private IMvxNavigationService _navigationService;
-        private IVerifier _verifier;
+	public class CertifyViewModel : MvxViewModel<IVerifier>
+	{
+		public ID ID { get; set; }
+		public List<CheckedAttribute> Attributes { get; set; }
+		public List<string> _attributesToCertify;
+		private IMvxNavigationService _navigationService;
+		private IVerifier _verifier;
 
-        public CertifyViewModel(IMvxNavigationService navigationService)
-        {
-            _navigationService = navigationService;
-            _attributesToCertify = new List<string>();
-        }
+		public class CheckedAttribute
+		{
+			private CertifyViewModel _certifyViewModel;
+			public string Description { get; set; }
+			private bool _isChecked;
 
-        public override Task Initialize(IVerifier verifier)
-        {
-            _verifier = verifier;
-            ID = _verifier.SynAck.Id;
-            Attributes = ID.Attributes.Values.ToList();
-            return Task.FromResult(true);
-        }
-        public IMvxCommand UpdateCertifiedAttributesCommand => new MvxCommand<string>(UpdateCertifiedAttributes);
+			public bool IsChecked
+			{
+				get { return _isChecked; }
+				set
+				{
+					_isChecked = value;
+					_certifyViewModel.UpdateCertifiedAttributes(Description);
+				}
+			}
 
-        public void UpdateCertifiedAttributes(string attributeDescription)
-        {
-            if (_attributesToCertify.Contains(attributeDescription))
-            {
-                _attributesToCertify.Remove(attributeDescription);
-            }
-            else
-            {
-                _attributesToCertify.Add(attributeDescription);
-            }
-        }
+			public CheckedAttribute(CertifyViewModel certifyViewModel, bool check, string attrDescription)
+			{
+				_certifyViewModel = certifyViewModel;
+				_isChecked = check;
+				Description = attrDescription;
+			}
+		}
 
-        public IMvxCommand CertifyIDCommand => new MvxCommand(CertifyID);
+		public CertifyViewModel(IMvxNavigationService navigationService)
+		{
+			_navigationService = navigationService;
+		}
 
-        private void CertifyID()
-        {
-            _verifier.Certify(_attributesToCertify.ToArray());
-            _verifier.CertificatesSent += (sender, e) => { Close((this)); };
-        }
-    }
+		public override Task Initialize(IVerifier verifier)
+		{
+			_verifier = verifier;
+			ID = _verifier.SynAck.Id;
+			Attributes = new List<CheckedAttribute>();
+			foreach (Attribute attr in ID.Attributes.Values)
+			{
+				Attributes.Add(new CheckedAttribute(this, false, attr.Description));
+			}
+			_attributesToCertify = new List<string>();
+			return base.Initialize();
+		}
+
+		public override void Start()
+		{
+			//TODO: Add starting logic here
+		}
+
+		private void UpdateCertifiedAttributes(string attributeDescription)
+		{
+			if (_attributesToCertify.Contains(attributeDescription))
+			{
+				_attributesToCertify.Remove(attributeDescription);
+			}
+			else
+			{
+				_attributesToCertify.Add(attributeDescription);
+			}
+		}
+
+		public IMvxCommand CertifyIDCommand => new MvxCommand(CertifyID);
+
+		private void CertifyID()
+		{
+			_verifier.Certify(_attributesToCertify.ToArray());
+		}
+	}
 }
--- Lynx.Mobile/Lynx.Droid/Views/CertifyView.cs
+++ Lynx.Mobile/Lynx.Droid/Views/CertifyView.cs
@@ -1,20 +1,18 @@
 ﻿using System;
 using Android.App;
 using Android.OS;
-using Android.Support.Design.Widget;
 using Lynx.Core.ViewModels;
 using MvvmCross.Binding.BindingContext;
 using MvvmCross.Droid.Support.V4;
-using ZXing.Net.Mobile.Android;
 
 namespace Lynx.Droid.Views
 {
-    [Activity(Label = "View for CertifyViewModel")]
-    public class CertifyView : MvxFragmentActivity
-    {
-        public CertifyView()
-        {
-        }
+	[Activity(Label = "View for CertifyViewModel")]
+	public class CertifyView : MvxFragmentActivity
+	{
+		public CertifyView()
+		{
+		}
 
 		protected override void OnCreate(Bundle bundle)
 		{
@@ -26,6 +24,7 @@ namespace Lynx.Droid.Views
 		private void BindCommand()
 		{
 			var set = this.CreateBindingSet<CertifyView, CertifyViewModel>();
+			set.Apply();
 		}
-    }
+	}
 }
