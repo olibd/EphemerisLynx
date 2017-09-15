1a789c581c9116b5b284ac7a450da7082cb11741
Olivier Brochu Dufour <olibd@outlook.com>
2017-09-15 18:36:14 -0400

    The checkedAttribute class now takes in the actual attribute and acts a proxy

--- Lynx.Mobile/Lynx.Core/ViewModels/CertifyViewModel.cs
+++ Lynx.Mobile/Lynx.Core/ViewModels/CertifyViewModel.cs
@@ -11,79 +11,81 @@ using Lynx.Core.PeerVerification.Interfaces;
 
 namespace Lynx.Core.ViewModels
 {
-	public class CertifyViewModel : MvxViewModel<IVerifier>
-	{
-		public ID ID { get; set; }
-		public List<CheckedAttribute> Attributes { get; set; }
+    public class CertifyViewModel : MvxViewModel<IVerifier>
+    {
+        public ID ID { get; set; }
+        public List<CheckedAttribute> Attributes { get; set; }
         private List<string> _attributesToCertify;
-		private IMvxNavigationService _navigationService;
-		private IVerifier _verifier;
+        private IMvxNavigationService _navigationService;
+        private IVerifier _verifier;
 
-		public class CheckedAttribute
-		{
-			private CertifyViewModel _certifyViewModel;
-			public string Description { get; set; }
-			private bool _isChecked;
+        public class CheckedAttribute
+        {
+            private CertifyViewModel _certifyViewModel;
+            public string Description { get { return attr.Description; } }
+            public IContent Content { get { return attr.Content; } }
+            private bool _isChecked;
+            private Attribute attr;
 
-			public bool IsChecked
-			{
-				get { return _isChecked; }
-				set
-				{
-					_isChecked = value;
-					_certifyViewModel.UpdateCertifiedAttributes(Description);
-				}
-			}
+            public bool IsChecked
+            {
+                get { return _isChecked; }
+                set
+                {
+                    _isChecked = value;
+                    _certifyViewModel.UpdateCertifiedAttributes(Description);
+                }
+            }
 
-			public CheckedAttribute(CertifyViewModel certifyViewModel, bool check, string attrDescription)
-			{
-				_certifyViewModel = certifyViewModel;
-				_isChecked = check;
-				Description = attrDescription;
-			}
-		}
+            public CheckedAttribute(CertifyViewModel certifyViewModel, bool check, Attribute attribute)
+            {
+                _certifyViewModel = certifyViewModel;
+                _isChecked = check;
+                attr = attribute;
+            }
+        }
 
-		public CertifyViewModel(IMvxNavigationService navigationService)
-		{
-			_navigationService = navigationService;
-		}
+        public CertifyViewModel(IMvxNavigationService navigationService)
+        {
+            _navigationService = navigationService;
+        }
 
-		public override Task Initialize(IVerifier verifier)
-		{
-			_verifier = verifier;
-			ID = _verifier.SynAck.Id;
-			Attributes = new List<CheckedAttribute>();
-			foreach (Attribute attr in ID.Attributes.Values)
-			{
-				Attributes.Add(new CheckedAttribute(this, false, attr.Description));
-			}
-			_attributesToCertify = new List<string>();
-			return base.Initialize();
-		}
+        public override Task Initialize(IVerifier verifier)
+        {
+            _verifier = verifier;
+            ID = _verifier.SynAck.Id;
+            Attributes = new List<CheckedAttribute>();
+            foreach (Attribute attr in ID.Attributes.Values)
+            {
+                Attributes.Add(new CheckedAttribute(this, false, attr));
+            }
+            _attributesToCertify = new List<string>();
+            return base.Initialize();
+        }
 
-		public override void Start()
-		{
-			//TODO: Add starting logic here
-		}
+        public override void Start()
+        {
+            //TODO: Add starting logic here
+        }
 
-		private void UpdateCertifiedAttributes(string attributeDescription)
-		{
-			if (_attributesToCertify.Contains(attributeDescription))
-			{
-				_attributesToCertify.Remove(attributeDescription);
-			}
-			else
-			{
-				_attributesToCertify.Add(attributeDescription);
-			}
-		}
+        private void UpdateCertifiedAttributes(string attributeDescription)
+        {
+            if (_attributesToCertify.Contains(attributeDescription))
+            {
+                _attributesToCertify.Remove(attributeDescription);
+            }
+            else
+            {
+                _attributesToCertify.Add(attributeDescription);
+            }
+        }
 
-		public IMvxCommand CertifyIDCommand => new MvxCommand(CertifyID);
+        public IMvxCommand CertifyIDCommand => new MvxCommand(CertifyID);
 
-		private void CertifyID()
-		{
-			_verifier.Certify(_attributesToCertify.ToArray());
+        private void CertifyID()
+        {
+            _verifier.Certify(_attributesToCertify.ToArray());
             _verifier.CertificatesSent += (sender, e) => { Close((this)); };
-		}
-	}
+        }
+    }
 }
