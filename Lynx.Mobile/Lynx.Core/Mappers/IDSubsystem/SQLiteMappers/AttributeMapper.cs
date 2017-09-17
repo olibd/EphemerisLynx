685b3bad40d0359cfa42105b861770bca7700772
Jonathan Montineri <jmontineri@gmail.com>
2017-09-16 20:10:28 -0400

    Mapper fixes

--- Lynx.Mobile/Lynx.Core/Mappers/IDSubsystem/SQLiteMappers/AttributeMapper.cs
+++ Lynx.Mobile/Lynx.Core/Mappers/IDSubsystem/SQLiteMappers/AttributeMapper.cs
@@ -4,6 +4,7 @@ using System.Threading.Tasks;
 using Lynx.Core.Mappers.IDSubsystem.Strategies;
 using Lynx.Core.Models;
 using Lynx.Core.Models.IDSubsystem;
+using SQLite;
 using Attribute = Lynx.Core.Models.IDSubsystem.Attribute;
 
 namespace Lynx.Core.Mappers.IDSubsystem.SQLiteMappers
@@ -36,8 +37,11 @@ namespace Lynx.Core.Mappers.IDSubsystem.SQLiteMappers
         /// <param name="obj">Object.</param>
         private async Task SaveCertificates(Attribute obj)
         {
-            if (obj.Certificates == null)
+            if (obj.Certificates == null || obj.Certificates.Count == 0)
+            {
+                await CreateTableAsync<AttributeCertificateMapping>();
                 return;
+            }
 
             //for each certificate in the attribute
             foreach (KeyValuePair<string, Certificate> entry in obj.Certificates)
@@ -59,6 +63,39 @@ namespace Lynx.Core.Mappers.IDSubsystem.SQLiteMappers
         }
 
         /// <summary>
+        /// Get object T given the UID.
+        /// </summary>
+        /// <returns>The object T.</returns>
+        /// <param name="UID">UID.</param>
+        public override async Task<Attribute> GetAsync(int UID)
+        {
+            Attribute attr = await base.GetAsync(UID);
+
+            //if we have a non-empty collection of certificates, then we return
+            //because it means that the Attribute was loaded from the identity
+            //map
+            if (attr.Certificates.Count > 0)
+                return attr;
+
+            return await Task.Run(async () =>
+            {
+                SQLiteConnection conn = new SQLiteConnection(_dbFilePath);
+                TableQuery<AttributeCertificateMapping> query = conn
+                    .Table<AttributeCertificateMapping>()
+                    .Where(mapping => mapping.AttrUID == UID);
+
+                foreach (AttributeCertificateMapping mapping in query)
+                {
+                    Certificate cert = await _certMapper.GetAsync(mapping.CertUID);
+                    cert.OwningAttribute = attr;
+                    attr.AddCertificate(cert);
+                }
+
+                return attr;
+            });
+        }
+
+        /// <summary>
         /// Attribute-certificate mapping. Creates a one to many relationship in
         /// the DB between an attribute and its certificates. Does not create
         /// foreign key.
@@ -68,6 +105,7 @@ namespace Lynx.Core.Mappers.IDSubsystem.SQLiteMappers
             private int _attrUID;
             private int _certUID;
 
+            [Indexed]
             public int AttrUID
             {
                 get { return _attrUID; }
