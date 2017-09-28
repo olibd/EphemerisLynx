d0123cd606a2bb84ed741ab388404bbff14bd438
Olivier Brochu Dufour <olibd@outlook.com>
2017-09-28 13:39:45 -0400

    Created the models for the API

---
+++ Lynx.Mobile/Lynx.API/Models/Client.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections.Generic;
+using Microsoft.AspNetCore.Identity;
+
+namespace Lynx.API.Models
+{
+    public class Client : IdentityUser
+    {
+        public string APIKey { get; set; }
+        public int IDUID { get; set; }
+        public string PrivateKey { get; set; }
+        public ICollection<Session> Sessions { get; set; }
+        public Client()
+        {
+            Sessions = new List<Session>();
+        }
+    }
+}
---
+++ Lynx.Mobile/Lynx.API/Models/SessionContext.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+
+namespace Lynx.API.Models
+{
+    public class SessionContext : DbContext
+    {
+        public SessionContext(DbContextOptions<SessionContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Session> Sessions { get; set; }
+    }
+}
