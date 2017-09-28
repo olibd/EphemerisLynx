aba65fb8a7512830a3b4e7fdcfee031b28d893b1
Olivier Brochu Dufour <olibd@outlook.com>
2017-09-28 13:40:28 -0400

    Created a DTO to map the post request to an object that can be used by .NET core

---
+++ Lynx.Mobile/Lynx.API/Models/ReadRequestDTO.cs
@@ -0,0 +1,9 @@
+﻿using System;
+namespace Lynx.API.Models
+{
+    public class ReadRequestDTO
+    {
+        public string APIKey { get; set; }
+        public string CallbackEndpoint { get; set; }
+    }
+}
