efe4a656a7cf6f05089d17540bafd421b3e4f7eb
Olivier Brochu Dufour <olibd@outlook.com>
2017-10-17 13:20:31 -0400

    Added comment

--- Lynx.Mobile/Lynx.API/HangfireActivator.cs
+++ Lynx.Mobile/Lynx.API/HangfireActivator.cs
@@ -1,6 +1,11 @@
 ﻿using System;
 namespace Lynx.API
 {
+    /// <summary>
+    /// The Hangfire activator is a HangFire job activator which is used to activate
+    /// jobs while supplying the .NET Core ServiceProvider so that the job class
+    /// can resolve dependencies.
+    /// </summary>
     public class HangfireActivator : Hangfire.JobActivator
     {
         private readonly IServiceProvider _serviceProvider;
