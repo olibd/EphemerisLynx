d6af6f2a438ec47ae0e104224b609985161fd4b0
Jonathan Montineri <jmontineri@gmail.com>
2017-11-17 15:32:47 -0500

    Added user facing exception abstract class

---
+++ Lynx.Mobile/Lynx.CoreStandard/UserFacingException.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Lynx.Core
+{
+    public abstract class UserFacingException : Exception
+    {
+        protected UserFacingException()
+        {
+        }
+
+        protected UserFacingException(string message) : base(message)
+        {
+        }
+
+        protected UserFacingException(string message, Exception innerException) : base(message, innerException)
+        {
+        }
+    }
+}
