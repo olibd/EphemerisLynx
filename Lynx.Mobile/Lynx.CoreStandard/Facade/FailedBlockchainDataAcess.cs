5c84567ea9cee0878a34cd9241e28229e15d025f
Olivier Brochu Dufour <olibd@outlook.com>
2017-12-05 14:18:11 -0500

    Updated the exceptions available in the namespace

---
+++ Lynx.Mobile/Lynx.CoreStandard/Facade/FailedBlockchainDataAcess.cs
@@ -0,0 +1,18 @@
+﻿using System;
+namespace Lynx.Core.PeerVerification
+{
+    public class FailedBlockchainDataAcess : UserFacingException
+    {
+        public FailedBlockchainDataAcess()
+        {
+        }
+
+        public FailedBlockchainDataAcess(string message) : base(message)
+        {
+        }
+
+        public FailedBlockchainDataAcess(string message, Exception innerException) : base(message, innerException)
+        {
+        }
+    }
+}
